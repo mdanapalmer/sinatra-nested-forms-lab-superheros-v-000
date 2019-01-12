cbe47aeb8eead92963730ed2d6f4201b81df370b
Dana Palmer <roiai4@hotmail.com>
2019-01-12 18:05:46 +0000

    Automatically backed up by Learn

---
+++ app/models/hero.rb
@@ -0,0 +1,14 @@
+class Hero
+
+  attr_accessor :name, :power, :bio
+
+  @@heroes = []
+
+  def initialize (name, power, bio)
+    @name = name
+    @power = power
+    @bio = bio
+    @@heroes << self
+  end
+
+end
